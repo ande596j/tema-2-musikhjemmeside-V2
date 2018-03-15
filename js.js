087b8a764835eeda15373d2a862eedbe46ef703a
Niklas <niklaswadum1234@gmail.com>
2018-03-15 14:04:06 +0100

    420

--- js.js
+++ js.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
 
 
-        $('nav a,footer a.up').click(function(e){
-            $.scrollTo( this.hash || 0, 1500);
-            e.preventDefault();
-        })},
-    onclick="video.paused ? this.play() : this.pause();",
-    $(document).ready(function () {
-        $(window).on('load scroll', function () {
-            var scrolled = $(this).scrollTop();
-            $('#title').css({
-                'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)',
-                'opacity': 1 - scrolled / 400
-            });
-            $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
-        });
-
-
+    $('nav a,footer a.up').click(function(e){
+        $.scrollTo( this.hash || 0, 1500);
+        e.preventDefault();
     });
+
+    $(".Concerts").click(function() {
+        $('html, body').animate({scrollTop: $("#Concerts").offset().top - 78}, 2000);
+    });
+    $(".Booking").click(function() {
+        $('html, body').animate({scrollTop: $("#Booking").offset().top - 78}, 2000);
+    });
+    $(".News").click(function() {
+        $('html, body').animate({scrollTop: $("#News").offset().top - 78}, 2000);
+    });
+    $(".Gallery").click(function() {
+        $('html, body').animate({scrollTop: $("#Gallery").offset().top - 78}, 2000);
+    });
+    $(".Biography").click(function() {
+        $('html, body').animate({scrollTop: $("#Biography").offset().top - 78}, 2000);
+    });
+
+
+
+
+
+
+});
