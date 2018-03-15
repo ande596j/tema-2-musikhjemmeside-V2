956e8a5438d2c6920f09ae58ba13417426590729
Niklas <niklaswadum1234@gmail.com>
2018-03-15 15:52:44 +0100

    360

--- js.js
+++ js.js
@@ -7,19 +7,19 @@ $(document).ready(function(){
     });
 
     $(".Concerts").click(function() {
-        $('html, body').animate({scrollTop: $("#Concerts").offset().top - 78}, 2000);
+        $('html, body').animate({scrollTop: $("#Concerts").offset().top - 78}, 1000);
     });
     $(".Booking").click(function() {
-        $('html, body').animate({scrollTop: $("#Booking").offset().top - 78}, 2000);
+        $('html, body').animate({scrollTop: $("#Booking").offset().top - 78}, 1000);
     });
     $(".News").click(function() {
-        $('html, body').animate({scrollTop: $("#News").offset().top - 78}, 2000);
+        $('html, body').animate({scrollTop: $("#News").offset().top - 78}, 1000);
     });
     $(".Gallery").click(function() {
-        $('html, body').animate({scrollTop: $("#Gallery").offset().top - 78}, 2000);
+        $('html, body').animate({scrollTop: $("#Gallery").offset().top - 78}, 1000);
     });
     $(".Biography").click(function() {
-        $('html, body').animate({scrollTop: $("#Biography").offset().top - 78}, 2000);
+        $('html, body').animate({scrollTop: $("#Biography").offset().top - 78}, 1000);
     });
 
 
